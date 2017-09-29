28ae66d1e2201d009d19c1daf409b68a98afb46d
Marc Hampson <mhampson@wk360.com>
2017-09-29 13:53:26 +0100

    Initial Commit

---
+++ Multi Select Image Picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multi Select Image Picker
+//
+//  Created by Marc Hampson on 29/09/2017.
+//  Copyright © 2017 Marc Hampson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
